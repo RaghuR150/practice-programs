4ba24d5783db7f8f529ac30e080885c12c3d66e1
RaghuR150 <raghunhce2@gmail.com>
2026-02-25 14:40:47 +0530

    concat using streams

---
+++ src/practice/programs/ConcateListsUsingStreams.java
@@ -0,0 +1,21 @@
+package practice.programs;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ConcateListsUsingStreams
+{
+	public static void main(String[] args) {
+	List<Integer> list1 = Arrays.asList(1,2,3,4);
+	List<Integer> list2 = Arrays.asList(5,6,7,8);
+	
+	System.out.println(concatLists(list1,list2));
+	
+	}
+	
+	public static List<Integer> concatLists(List<Integer> list1, List<Integer> list2){
+	    return Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+	}
+}
