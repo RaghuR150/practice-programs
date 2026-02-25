570248dbb8604faaefbf8adc0843959dad8dfb35
RaghuR150 <raghunhce2@gmail.com>
2026-02-25 23:44:36 +0530

    find N th fibonacci number using recurssion

--- src/com/kunal/reccurance/Fibonacci.java
+++ src/com/kunal/reccurance/Fibonacci.java
@@ -1,11 +1,11 @@
-package com.kunal;
+package com.kunal.reccurance;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int ans = fibonacci(5);
+        int ans = fibonacci(8);
         System.out.println(ans);
     }
-
+    // inefficient for large numbers
     public static int fibonacci(int n){
         if(n<2){
             return n;
