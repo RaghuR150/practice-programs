976c998994e67e138b2972d43edbc8d36d6f406b
RaghuR150 <raghunhce2@gmail.com>
2026-02-25 12:41:24 +0530

    anagram using streams

---
+++ src/practice/programs/anagram/Anagram.java
@@ -0,0 +1,17 @@
+package practice.programs.anagram;
+
+import java.util.stream.Collectors;
+
+public class Anagram
+{
+	public static void main(String[] args) {
+		String s1 = "listen";
+		String s2 = "silent";
+		System.out.println(isAnagram(s1,s2));
+	}
+	
+	public static boolean isAnagram(String s1, String s2){
+	    return s1.chars().sorted().boxed().collect(Collectors.toList())
+	    .equals(s2.chars().sorted().boxed().collect(Collectors.toList()));
+	}
+}
