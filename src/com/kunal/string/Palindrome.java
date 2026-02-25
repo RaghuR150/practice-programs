ba337a73d3f33c6f1613b96aa83707f7292109cb
RaghuR150 <raghunhce2@gmail.com>
2026-02-26 00:31:57 +0530

    check if the string is a palindrome or not

---
+++ src/com/kunal/string/Palindrome.java
@@ -0,0 +1,19 @@
+package com.kunal.string;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+
+    static boolean isPalindrome(String str){
+
+        for(int i=0; i<= str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
